93fc070ebc8888fe4ca0118f83b14247c812b1d7
xsaven <gms.sw.xsaven@gmail.com>
2025-05-22 02:59:02 +0300

    README.md, composer.json, install.sh

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ### Installation
 To install the skeleton, you can use the following command:
 ```bash
-curl -fsSL 'https://raw.githubusercontent.com/Xsaven/getorder-skeleton/master/install.sh' | NODE_NAME=my-node bash
+curl -fsSL 'https://raw.githubusercontent.com/Xsaven/getorder-skeleton/master/install.sh' | \
+  NODE_NAME=my-node  \
+  DB_USERNAME=admin  \
+  DB_PASSWORD=admin  \
+  bash
 ```
 
--- install.sh
+++ install.sh
@@ -6,11 +6,3 @@ REPO_URL="git@github.com:Xsaven/getorder-skeleton.git"
 composer create-project getorder/skeleton "$NODE_NAME" \
   --repository="{\"type\":\"vcs\",\"url\":\"$REPO_URL\"}" \
   --stability=dev
-cd "$NODE_NAME" || exit 1
-composer install
-composer dump-autoload
-git init
-php artisan microservice:prepare-stubs
-php artisan microservice:prepare-name "$NODE_NAME"
-read -rp "Введіть JWT сікрет (або залешіть пусти для автогенерації): " JWT_SECRET
-php artisan microservice:prepare-jwt-secret "$JWT_SECRET"
